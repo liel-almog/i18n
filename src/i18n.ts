49d362a6ba03540e624bab0ddd547e00fa4ac47d
Liel Almog <liel.almogg@gmail.com>
2023-04-08 01:28:19 +0300

    Feat: Install i18n HTTP backend to load translation files with HTTP Add all translation files to public folder so they will be served with HTTP. Configure the loadPath in the backend option

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,37 +1,26 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-import HelloWorldEN_US from './locales/en-US/HelloWorld.json';
-import About_EN_US from './locales/en-US/About.json';
-import HelloWorldHE_IL from './locales/he-IL/HelloWorld.json';
-import About_HE_IL from './locales/he-IL/About.json';
-
-export const resources = {
-    'en-US': {
-        HelloWorld: HelloWorldEN_US,
-        About: About_EN_US,
-    },
-    'he-IL': {
-        HelloWorld: HelloWorldHE_IL,
-        About: About_HE_IL,
-    },
-} as const;
 
 i18next
     .use(LanguageDetector)
     .use(initReactI18next)
+    .use<HttpApi>(HttpApi)
     .init({
         fallbackLng: 'he-IL',
         interpolation: {
             escapeValue: false, // react already safes from xss
         },
-
-        cache: ['localStorage'],
-        resources,
+        defaultNS: 'common',
+        cache: ['localStorage', 'cookie'],
         detection: {
             lookupLocalStorage: 'i18nextLng',
         },
         debug: true,
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
     });
 
 export default i18next;
